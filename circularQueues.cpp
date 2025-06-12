a8a597f5b5f1e74662ef6bc18ed6fd0dde98557c
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-06-12 17:37:46 +0700

    Prosedur insert

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -14,4 +14,12 @@ class Queues
             FRONT = -1;
             REAR = -1;
         }
+
+        void insert()
+        {
+            int num;
+            cout << "Enter a  number: ";
+            cin >> num;
+            cout << endl;
+        }
 };
